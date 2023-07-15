ed374ed370cc443f54c4873ad8b35a0cfd32813e
bp-statar001 <statarinov@productengine.com>
2023-07-15 22:54:59 +0300

    fifth commit

---
+++ src/main/real-12-dz/Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+    public static void main(String[] args) {
+        int x = 5;
+        String s = "Hello"; //Ссылочный
+        String space = " ";
+        String name = "Bob";
+        System.out.println(s+space+name);
+        System.out.println("Hello"+ " "+ "John");
+        System.out.println("My number is "+x);
+
+    }
+}
