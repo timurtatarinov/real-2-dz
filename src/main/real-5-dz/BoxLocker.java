c6191a2a0c6c9003eb74aa11b385d476c5ad8feb
bp-statar001 <statarinov@productengine.com>
2023-07-15 20:50:14 +0300

    fifth commit

---
+++ src/main/real-5-dz/BoxLocker.java
@@ -0,0 +1,85 @@
+import javafx.util.Builder;
+
+import java.util.Objects;
+
+public class BoxLocker {
+
+    private int lenght; //sm
+    private int width;  //sm
+    private int height; //sm
+    private int cost;   //₴
+    private String color;
+
+    /*public int getLenght() {
+        return lenght;
+    }
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public int getCost() {
+        return cost;
+    }
+    public String getColor() {
+        return color;
+    }*/
+
+    public static class Builder {
+        private BoxLocker boxLocker;
+
+        public Builder() {
+            boxLocker = new BoxLocker();
+        }
+
+        public Builder forLenght(int lenght) {
+            boxLocker.lenght = lenght;
+            return this;
+        }
+        public Builder forWidth(int width) {
+            boxLocker.width = width;
+            return this;
+        }
+        public Builder forHeight(int height) {
+            boxLocker.height = height;
+            return this;
+        }
+        public Builder forCost(int cost) {
+            boxLocker.cost = cost;
+            return this;
+        }
+        public Builder forColor(String color) {
+            boxLocker.color = color;
+            return this;
+        }
+
+        public BoxLocker build() {
+            return boxLocker;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BoxLocker boxLocker = (BoxLocker) o;
+
+        if (lenght != this.lenght) return false;
+        if (width != this.width) return false;
+        if (height != this.height) return false;
+        if (cost != this.cost) return false;
+        return color.equals(this.color);
+    }
+
+
+
+    @Override
+    public int hashCode(){
+        int result = color == null ? 0 : color.hashCode();
+        result = result + lenght + width + height + cost;
+        return result;
+    }
+
+}
---
+++ src/main/real-5-dz/main_5_dzs.java
@@ -0,0 +1,36 @@
+public class main_5_dzs {
+
+    public static void main(String[] args) {
+
+        BoxLocker boxLocker1 = new BoxLocker.Builder()
+                .forLenght(40)
+                .forWidth(40)
+                .forHeight(60)
+                .forCost(600)
+                .forColor("White Wood")
+                .build();
+        BoxLocker boxLocker2 = new BoxLocker.Builder()
+                .forLenght(40)
+                .forWidth(40)
+                .forHeight(60)
+                .forCost(600)
+                .forColor("White Wood")
+                .build();
+
+        if (boxLocker1.hashCode() == boxLocker2.hashCode()) {
+            System.out.println("Хеш-коды этих объектов одинаковы.");
+
+            if (boxLocker1.equals(boxLocker2)) {
+                System.out.println("Сами объекты тоже одинаковы.");
+            } else {
+                System.out.println("А сами объекты неодинаковы.");
+            }
+
+        } else {
+            System.out.println("Хеш-коды этих объектов неодинаковы." + "\n"
+            + "А следовательно, и объекты тоже.");
+        }
+
+    }
+
+}
